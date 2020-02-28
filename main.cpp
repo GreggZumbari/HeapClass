94bb97a41a370f09eeb11fafccca1bbf72dc6c00
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2020-02-28 10:49:18 -0800

    10:49 2/28/2020

--- main.cpp
+++ main.cpp
@@ -5,10 +5,28 @@
 
 #include "GHeap.h"
 
+#define LEN 10000
+
 using namepsace std;
 
 int main() {
 	
+	char* in = new int[LEN];
 	
-	
+	cout << "Welcome to The Heap Shack! How may I take your order?" << endl
+	<< "(Type \"console\" to type from console, Type \"file\" to read from file)" << endl;
+
+	cin.getline(in, LEN);
+
+	//Read from console
+	if (strcmp(in, "console") == 0 || strcmp(in, "c") == 0 || strcmp(in, "C") == 0) {
+		
+	}
+}
+
+void clearCString(char*& in) {
+	for (int i = 0; i < strlen(in); i++) {
+		in[i] = '\0';
+	}
+	return in;
 }
