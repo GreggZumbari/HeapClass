8178801e09c88a435273320099ac95e769c93ce1
Gregg <greggory.png@gmail.com>
2020-03-01 20:44:08 -0800

    8:42 PM 3/1/2020
    Can read in from file or console correctly

--- main.cpp
+++ main.cpp
@@ -5,28 +5,84 @@
 
 #include "GHeap.h"
 
-#define LEN 10000
+#define LEN 100
+#define BIGLEN 10000
 
-using namepsace std;
+using namespace std;
+
+void clearCString(char*& cmdin);
 
 int main() {
 	
-	char* in = new int[LEN];
+	char* cmdin = new char[LEN];
+	char* numin = new char[BIGLEN];
 	
-	cout << "Welcome to The Heap Shack! How may I take your order?" << endl
-	<< "(Type \"console\" to type from console, Type \"file\" to read from file)" << endl;
-
-	cin.getline(in, LEN);
-
-	//Read from console
-	if (strcmp(in, "console") == 0 || strcmp(in, "c") == 0 || strcmp(in, "C") == 0) {
+	cout << "Welcome to Gregg\'s fabulous heap program" << endl;
+	
+	while (true) {
+		clearCString(cmdin);
+	
+		cout << "Type \"console\" to type from console " << endl << 
+		"Type \"file\" to read from file " << endl << 
+		"Type \"exit\" to kill the program" << endl;
+	
+		cout << "Horp> ";
+		cin >> cmdin; cin.clear(); cin.ignore(LEN, '\n');
 		
+		cout << endl;
+		//Read from console
+		if (strcmp(cmdin, "console") == 0 || strcmp(cmdin, "c") == 0 || strcmp(cmdin, "C") == 0) {
+			cout << "Reading from console..." << endl <<
+			"Input your number inputs, each separated by a space" << endl;
+			
+			cout << "Horp> ";
+			cin.getline(numin, LEN);
+	
+			//At this point, we have our input
+			cout << numin << endl;
+		}
+		//Read from file
+		else if (strcmp(cmdin, "file") == 0 || strcmp(cmdin, "f") == 0 || strcmp(cmdin, "F") == 0) {
+			char* add = new char[LEN];
+			FILE* fin; //Credit to my dad from showing me the FILE libraries from ye olde C
+			
+			clearCString(fileCont);
+			clearCString(add);
+			
+			cout << "Reading from file..." << endl <<
+			"Input the path to your file. It must be a text file:" << endl;
+			
+			//Get address
+			cout << "File> ";
+			cin.getline(add, LEN);
+			
+			//Get file from address
+			fin = fopen(add, "r"); //"r" for read
+			fgets(fileCont, LEN, fin);
+			fclose(fin);
+			
+			cout << "File contents: " << fileCont << endl;
+			
+			delete(add);
+			delete(fileCont);
+		}
+		//Exit
+		else if (strcmp(cmdin, "exit") == 0 || strcmp(cmdin, "e") == 0 || strcmp(cmdin, "E") == 0) {
+			cout << "Closing program..." << endl;
+			break;
+		}
+		//Invalid command
+		else {
+			cout << "Command \"" << cmdin << "\" not recognized" << endl;
+		}
+		cout << endl;
 	}
+	
+	return 0;
 }
 
-void clearCString(char*& in) {
-	for (int i = 0; i < strlen(in); i++) {
-		in[i] = '\0';
+void clearCString(char*& cmdin) {
+	for (int i = 0; i < strlen(cmdin); i++) {
+		cmdin[i] = (char)0;
 	}
-	return in;
 }
